acc510f00f63b7940cd648f4c1b9e64f36798e9a
destinylover <157787946+destinylover@users.noreply.github.com>
2025-04-23 20:08:04 +0900

    [Bronze III] Title: 학점계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,30 @@
+A = input()
+
+if A == "A0":
+    print("4.0")
+elif A == "A+":
+    print("4.3")
+elif A == "A-":
+    print("3.7")
+elif A == "B+":
+    print("3.3")
+elif A == "B0":
+    print("3.0")
+elif A == "B-":
+    print("2.7")
+elif A == "C+":
+    print("2.3")
+elif A == "C0":
+    print("2.0")
+elif A == "C-":
+    print("1.7")
+elif A == "D+":
+    print("1.3")
+elif A == "D0":
+    print("1.0")
+elif A == "D-":
+    print("0.7")
+elif A == "F":
+    print("0.0")
+else:
+    print("잘못입력했습니다.")
