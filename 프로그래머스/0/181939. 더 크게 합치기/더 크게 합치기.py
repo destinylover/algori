a1cc87ae8eb1baee266e68df3bbfb96d2009837c
SEOHO DONG <157787946+destinylover@users.noreply.github.com>
2026-05-16 02:41:28 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    answer = 0
+    ab = str(a) + str(b)
+    ba = str(b) + str(a)
+    if int(ab) > int(ba):
+        answer = int(ab)
+    else:
+        answer = int(ba)
+        
+    return answer
